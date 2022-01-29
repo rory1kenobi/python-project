5523f8ba23186e25949dc4f1b005d5a99303fdc2
rory <rory-smith14@hotmail.co.uk>
2022-01-29 16:25:43 +0000

    added some css to home page

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app.register_blueprint(bucket_list_blueprint)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
